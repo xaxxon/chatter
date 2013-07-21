217d50e98b523393f614466f9711359cc1493b09
Zac Hansen <xaxxon@Zacs-Mac-3.local>
2013-07-20 18:32:43 -0700

    Moved around socket read/write to game object from user object

--- socket.rb
+++ socket.rb
@@ -1,7 +1,6 @@
 require 'socket'
 
 
-
 class LoginProcess
   
   def initialize(game, user)
@@ -24,15 +23,15 @@ end
 class User
   
   attr_reader :socket
-  attr_accessor :name
+  attr_accessor :send_buffer
   
-  def name(name = nil)
-    if name != nil
-      raise "Cannot change name" if @name != nil
-      @name = name
-    else
-      @name
-    end
+  def name=(name)
+    raise "Cannot change name" if @name != nil
+    @name = name
+  end
+  
+  def name
+    return @name
   end
   
   def logged_in?
@@ -54,49 +53,35 @@ class User
     @game.watch_user_for_write self
   end
   
-  # actually sends data to client with a single call to write
-  def write_to_socket
-    bytes_written = socket.write_nonblock(@send_buffer)
-    @send_buffer = @send_buffer[bytes_written..-1]
-    return @send_buffer.size
+  
+  
+  def disconnect(remote_disconnect: false )
+      
   end
   
   
-  # returns whether the user has disconnected
-  def handle_input
-    begin
-      begin
-        # read the data, but handle EOF if the client disconnected
-        data = socket.read_nonblock 4096
-      rescue EOFError => e
-        return true;
-      rescue Exception => e
-        puts "Unknown read error: ", e
-        p e.backtrace
-      end
-        
-      @input_buffer << data
-      p "input buffer now: ", @input_buffer
-      @input_buffer.each_line{|line|
-        puts "Dealing with line #{line}"
-        if line.end_with? $/
-          line.chomp!
-          puts "process count #{@processes.size}"
-          if @processes.empty?
-            puts "Sending to game #{@game} handle input"
-            @game.handle_input self, line
-          else
-            process_complete = @processes[0].handle_input line
-            if process_complete
-              @processes.shift
-              puts "#{@processes.size} processes remaining"
-            end
+  def handle_input(data)
+    
+    @input_buffer << data
+
+    @input_buffer.each_line{|line|
+
+      if line.end_with? $/
+        line.chomp!
+
+        if @processes.empty?
+          @game.handle_input self, line
+        else
+          process_complete = @processes[0].handle_input line
+          if process_complete
+            @processes.shift
           end
-        end   
-      }
-      p "done with lines"
-    end
-    return false; # user has not disconnected
+        end
+      else
+        @input_buffer = line
+        break
+      end
+    }
   end
     
 end
@@ -129,61 +114,75 @@ class Game
       users.select!{|user|user != flags[:not_user]}
     end
     
-    puts "get_users returning #{users.size} users"
     users
       
   end
+  
 
   def handle_input(user, line)
-    puts "Handing input for #{user.name} data #{line}"
-    get_users(:logged_in => true, :not_user => user).each{|user| user.send line}
+    get_users(:logged_in => true, :not_user => user).each{|each_user| each_user.send line}
   end
+
 
   # Have to call this to send to a user, because this tracks whether a user's socket needs
   #   to be selected for writing
   def watch_user_for_write(user)
     @sockets_with_writes_pending[user.socket] = 1;
-    puts "watching #{@sockets_with_writes_pending.size}"
   end
+  
+  
+  def handle_accept(server_socket)
+      client_socket = @server_socket.accept_nonblock
+      new_user = User.new(self, client_socket)
+      @socket_user_map[client_socket] = new_user    
+  rescue Exception => e
+    puts "Unknown accept error: ", e.inspect
+  end
+
+
+  def handle_client_input(socket)
+    # look up user object for this socket
+    user = @socket_user_map[socket];
+    # read the data, but handle EOF if the client disconnected
+    data = socket.read_nonblock 4096
+    
+    user.handle_input data
+
+  rescue EOFError
+    # handle any application-level cleanup for the user leaving
+    user.disconnect(:remote_disconnect => true)
+  
+    # remove the user from the select sockets - read and write
+    @socket_user_map.delete socket
+    @sockets_with_writes_pending.delete socket
+  end
+
 
   def go
     while true do
 
-      puts "calling select"
       reads, writes = IO.select(@socket_user_map.keys + [@server_socket], @sockets_with_writes_pending.keys);
-      puts "Back from select"
 
       # handle writes
       writes.each{|socket|
-        @sockets_with_writes_pending.delete(socket) unless @socket_user_map[socket].write_to_socket > 0
+        user = @socket_user_map[socket]
+        send_buffer = user.send_buffer
+        
+        bytes_written = socket.write_nonblock(send_buffer)
+        
+        if bytes_written = send_buffer.size
+          @sockets_with_writes_pending.delete(socket)
+        end
+        user.send_buffer = send_buffer[bytes_written..-1]
       }
 
       reads.each{|socket|
 
         if socket === @server_socket
-          begin
-            client_socket = @server_socket.accept_nonblock
-          rescue Exception => e
-            puts "Unknown accept error: ", e.inspect
-          end
-    
-          new_user = User.new(self, client_socket)
-    
-          @socket_user_map[client_socket] = new_user
-    
+          handle_accept(socket)
         else
-    
-          # look up user object for this socket
-          user = @socket_user_map[socket];
-    
-          begin
-            disconnected = user.handle_input
-            @socket_user_map.delete socket if disconnected
-            next;
-          end
-    
+          handle_client_input(socket)
         end
-
       }
     end
   end
